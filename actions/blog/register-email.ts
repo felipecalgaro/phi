8b8be1558f6b6015394cba837bc5ef7badc93aa2
Felipe <pipecalgaro@gmail.com>
2026-03-06 10:47:54 +0100

    fix: remove unused server actions

--- actions/blog/register-email.ts
+++
@@ -1,9 +0,0 @@
-"use server";
-
-import z from "zod";
-
-export async function registerEmail(formData: FormData) {
-  const email = z.email().parse(formData.get("email"));
-
-  console.log("Registering email:", email);
-}
