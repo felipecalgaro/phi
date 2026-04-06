83a5f2b6cd2f96895b289355c74af1d82e6df020
Felipe <pipecalgaro@gmail.com>
2026-04-06 08:28:22 +0200

    feat: add resources

--- app/acing-aufnahmetest/lessons/page.tsx
+++ app/acing-aufnahmetest/lessons/page.tsx
@@ -4,17 +4,7 @@ import prisma from '@/lib/prisma';
 import { Download, FileText, MoveLeft } from 'lucide-react';
 import Image from 'next/image';
 import Link from 'next/link';
-
-const MATERIALS = [
-  {
-    name: 'Material 1',
-    url: '#',
-  },
-  {
-    name: 'Material 2',
-    url: '#',
-  }
-]
+import { RESOURCES } from '@/data/acing-aufnahmetest/resources';
 
 export const revalidate = 3600
 
@@ -45,7 +35,7 @@ export default async function Lessons() {
 
       <main className="max-w-5xl w-full px-6 py-8">
         <div className="max-w-5xl mx-auto px-6 py-10">
-          <h1 className="text-2xl font-semibold text-foreground tracking-tight">
+          <h1 className="text-2xl font-semibold text-foreground tracking-tight text-center">
             Lessons
           </h1>
         </div>
@@ -71,31 +61,46 @@ export default async function Lessons() {
             )
           })}
         </div>
-        <div className="max-w-5xl mx-auto px-6 py-10 mt-16">
-          <h1 className="text-2xl font-semibold text-foreground tracking-tight">
-            Materials & Resources
+        <div className="max-w-5xl px-6 py-10 mt-16">
+          <h1 className="text-2xl font-semibold text-foreground tracking-tight text-center">
+            Resources
           </h1>
         </div>
-        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4">
-          {MATERIALS.map((material) => (
-            <Link
-              key={material.name}
-              href={material.url}
-              className="text-left rounded-xl overflow-hidden bg-card border border-border"
-            >
-              <div className="aspect-video bg-muted relative">
-                <div className="absolute top-2 right-2 bg-black/70 text-white text-[10px] font-medium px-1.5 py-0.5 rounded">
-                  <Download className="size-4 inline-block" />
+        <div className="flex flex-col gap-16 mt-8">
+          {Object.entries(RESOURCES).map(([category, files]) => {
+            return (
+              <div key={category} className='flex flex-col gap-8'>
+                <h2 className="text-xl font-medium text-foreground tracking-tight mx-6">
+                  {category}
+                </h2>
+                <div className='grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4'>
+                  {files.map(file => {
+                    const fileUrl = `https://${env.CLOUDFRONT_DOMAIN}/resources/${file.slug}.pdf`
+
+                    return (
+                      <Link
+                        key={file.name}
+                        href={fileUrl}
+                        className="text-left rounded-xl overflow-hidden bg-card border border-border"
+                      >
+                        <div className="aspect-video bg-muted relative">
+                          <div className="absolute top-2 right-2 bg-black/70 text-white text-[10px] font-medium px-1.5 py-0.5 rounded">
+                            <Download className="size-4 inline-block" />
+                          </div>
+                          <FileText className="size-16 text-muted-foreground absolute inset-0 m-auto" />
+                        </div>
+                        <div className="px-3 py-4">
+                          <p className="text-sm font-medium text-foreground leading-snug line-clamp-2">
+                            {file.name}
+                          </p>
+                        </div>
+                      </Link>
+                    )
+                  })}
                 </div>
-                <FileText className="size-16 text-muted-foreground absolute inset-0 m-auto" />
               </div>
-              <div className="px-3 py-4">
-                <p className="text-sm font-medium text-foreground leading-snug line-clamp-2">
-                  {material.name}
-                </p>
-              </div>
-            </Link>
-          ))}
+            )
+          })}
         </div>
       </main>
     </div>
---
+++ data/acing-aufnahmetest/resources.ts
@@ -0,0 +1,33 @@
+export const RESOURCES = {
+  "Grammar Topics": [
+    {
+      name: "Adjective Declination",
+      slug: "adjective-declination",
+    },
+    {
+      name: "Conjunctions",
+      slug: "conjunctions",
+    },
+    {
+      name: 'Infinitive with "zu"',
+      slug: "infinitive-with-zu",
+    },
+    {
+      name: "Perfect Tense",
+      slug: "perfect-tense",
+    },
+    {
+      name: "Passive Voice",
+      slug: "passive-voice",
+    },
+    {
+      name: "Prepositions",
+      slug: "prepositions",
+    },
+    {
+      name: "Pronouns",
+      slug: "pronouns",
+    },
+  ],
+  "Mock Exams": [],
+};
